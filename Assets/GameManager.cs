586a048c77ec328d59cd1634659f155f75ae782b
Alex Mercer <120363131+AlexmMercer@users.noreply.github.com>
2023-12-06 12:50:34 +0300

    CommitNew

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -43,6 +43,6 @@ public class GameManager : MonoBehaviour
 
     public void ExitGame()
     {
-        Application.Quit();
+        SceneManager.LoadScene("MainMenuScene");
     }
 }
---
+++ Assets/MainMenuGameManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuGameManager : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
